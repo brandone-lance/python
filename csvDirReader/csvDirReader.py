48108a2065f247a02ddb65d6914c346af592fea1
brandone-lance <30734421+brandone-lance@users.noreply.github.com>
2017-08-10 01:51:16 -0500

    update csvdirreader for non-csv handling
    Simply skips non-csv and displays message at end.

--- csvDirReader/csvDirReader.py
+++ csvDirReader/csvDirReader.py
@@ -8,15 +8,26 @@ csvDir = input('Diretory containing CSV files: ')
 if '\\' not in csvDir[-1]:
     csvDir = csvDir + '\\'
 
+##  Create a list of filenames from directory of choice and
+##  Change to the directory
 csvFiles = os.listdir(csvDir)
 os.chdir(csvDir)
 
+##  Set our counters to zero...
 fileCounter = 0
+failCounter = 0
 
+##  If it's a proper csv, read into dataframe else iterate failcounter
 for currentFile in csvFiles:
-    varName = currentFile[0:-4]
-    commandEval = varName + '= pd.read_csv(\'' + currentFile + '\')'
-    exec(commandEval)
-    fileCounter = fileCounter + 1
+    if 'csv' in currentFile[-3:]:        
+        varName = currentFile[0:-4]
+        commandEval = varName + '= pd.read_csv(\'' + currentFile + '\')'
+        exec(commandEval)
+        fileCounter = fileCounter + 1
+    else:
+        failCounter = failCounter + 1
 
-print(str(fileCounter) + ' CSV files converted into dataframes')
+##  Print our results...
+print(str(fileCounter) + ' CSV files converted into dataframes.')
+if failCounter != 0:
+    print(str(failCounter) + ' file(s) failed to be converted.')
